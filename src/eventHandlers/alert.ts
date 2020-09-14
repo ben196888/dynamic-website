d3a665753ac235c655a225658068c0adfd407db8
Ben Liu <ben196888@gmail.com>
2020-09-14 23:35:16 +0700

    feat: add eventHandlers in render factory

---
+++ src/eventHandlers/alert.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-alert
+const alert = ({ message }: { message: string }) => window.alert(message);
+
+export default alert;
--- src/pageRenderer/RenderFactory.tsx
+++ src/pageRenderer/RenderFactory.tsx
@@ -1,6 +1,31 @@
-import React from 'react';
-import { ElementState, TreeNode } from '../store/reducers/types';
+import React, { Attributes } from 'react';
+import {
+  ElementState, ElementNode, TreeNode, Handler,
+} from '../store/reducers/types';
 import MyComponent from '../components/MyComponent';
+import alert from '../eventHandlers/alert';
+
+const mapToHandler: { [name: string]: (parameters: Handler['parameters']) => any } = {
+  [alert.name]: alert,
+};
+
+function getAttributesAndProps<P extends {}>(node: ElementNode): Attributes & P {
+  return Object.keys(node)
+    .reduce((attributesAndProps, key) => {
+      if (/^on[A-Z]/.test(key)) {
+        const handler: Handler = node[key];
+        return {
+          ...attributesAndProps,
+          // TODO: find an elegant way to deal with events
+          [key]: (/* event */) => mapToHandler[handler.name](handler.parameters),
+        };
+      }
+      return {
+        ...attributesAndProps,
+        [key]: node[key],
+      };
+    }, {} as (Attributes & P));
+}
 
 const mapToComponent: { [name: string]: () => JSX.Element; } = {
   [MyComponent.name]: MyComponent,
@@ -11,7 +36,8 @@ function factory(treeNode: (TreeNode | string), nodeMap: ElementState): JSX.Elem
     return treeNode;
   }
   const type = mapToComponent[treeNode.type] || treeNode.type;
-  const attributesAndProps = nodeMap[treeNode.key] || null;
+  const attributesAndProps = nodeMap[treeNode.key]
+    ? getAttributesAndProps(nodeMap[treeNode.key]) : null;
   const children = (treeNode.children || [])
     .map((child) => factory(child, nodeMap));
   return React.createElement(type, attributesAndProps, ...children);
